99796beb7a1f2c71360c386b145d9df1c6014d45
medixwork <medixwork@gmail.com>
2020-10-17 03:01:15 +0700

    'ms'

--- src/database/base/mysql-connection.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-module.exports = {
-    connectionLimit : 10,
-    host     : "103.1.236.126",
-    user     : "tiktok",
-    password : "Thang01652608118",
-    database : "dev_vmedic__aa6c",
-    port     : 3306,
-}
